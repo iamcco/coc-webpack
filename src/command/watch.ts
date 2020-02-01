e325da3e1b409bc6148c22ab377ebfc2409fc5ab
iamcco <ooiss@qq.com>
2020-02-01 13:54:37 +0800

    feat(command): add webpack.stop command

--- src/command/watch.ts
+++ src/command/watch.ts
@@ -1,75 +1,112 @@
-import { commands, workspace, Disposable, ExtensionContext, Uri } from 'coc.nvim'
+import { commands, workspace, Disposable, ExtensionContext, Uri, StatusBarItem } from 'coc.nvim'
+import Task from 'coc.nvim/lib/model/task'
 import path from 'path'
 import { existsSync } from 'fs'
 
 const patternWebpack = /ERROR\sin\s(?<filename>\S+)\s(?<line>\d+):(?<col>\d+)/
 const errorPattern = /ERROR\sin\s(?<filename>[^(]+)\((?<line>\d+),(?<col>\d+)\)/
 
+let subscriptions: Disposable[] = []
+
 export const errorList: any[] = []
 
 export function watchCommand(context: ExtensionContext) {
-  let statusItem = workspace.createStatusBarItem(1, { progress: true })
-  let task = workspace.createTask('WEBPACK')
+  let statusItem: StatusBarItem
+  let task: Task
   let cwd: string
-
-  task.onExit(code => {
-    if (code !== 0) {
-      workspace.showMessage(`Webpack exit with code ${code}`, 'warning')
+  const watchSubscription = Disposable.create(() => {
+    if (subscriptions && subscriptions.length) {
+      subscriptions.forEach(sub => {
+        sub.dispose()
+      })
+      subscriptions = []
     }
-    statusItem.hide()
   })
 
-  task.onStdout(lines => {
-    let i = 0
-    let items = []
-    for (let line of lines) {
-      if (line.indexOf('ERROR') !== -1) {
-        let res = patternWebpack.exec(line)
-        if (res == null) {
-          res = errorPattern.exec(line)
-        }
-        if (res != null) {
-          let { filename } = res.groups!
-          if (!path.isAbsolute(filename)) {
-            filename = path.join(cwd, filename)
+  context.subscriptions.push(watchSubscription)
+
+  function newWatch () {
+    statusItem = workspace.createStatusBarItem(1, { progress: true })
+    task = workspace.createTask('WEBPACK')
+    subscriptions.push(task)
+    subscriptions.push(statusItem)
+
+    task.onExit(code => {
+      if (code !== 0) {
+        workspace.showMessage(`Webpack exit with code ${code}`, 'warning')
+      }
+      statusItem.hide()
+    })
+
+    task.onStdout(lines => {
+      let i = 0
+      let items = []
+      for (let line of lines) {
+        if (line.indexOf('ERROR') !== -1) {
+          let res = patternWebpack.exec(line)
+          if (res == null) {
+            res = errorPattern.exec(line)
           }
-          items.push({
-            name: path.relative(cwd, filename),
-            filename,
-            lnum: parseInt(res.groups!.line),
-            col: parseInt(res.groups!.col),
-            text: lines[i + 1].trim(),
-            type: 'E'
-          })
+          if (res != null) {
+            let { filename } = res.groups!
+              if (!path.isAbsolute(filename)) {
+                filename = path.join(cwd, filename)
+              }
+            items.push({
+              name: path.relative(cwd, filename),
+              filename,
+              lnum: parseInt(res.groups!.line),
+              col: parseInt(res.groups!.col),
+              text: lines[i + 1].trim(),
+              type: 'E'
+            })
+          }
+        }
+        i++
+      }
+      errorList.splice(0, errorList.length)
+      errorList.push(...items)
+      statusItem.text = items.length === 0 ? '✓ webpack' : '✗ webpack'
+      statusItem.isProgress = false
+    })
+
+    task.onStderr(lines => {
+      for (let line of lines) {
+        if (line.match(/webpack\sis\swatching/)) {
+          statusItem.isProgress = true
+          statusItem.text = 'webpack'
         }
       }
-      i++
-    }
-    errorList.splice(0, errorList.length)
-    errorList.push(...items)
-    statusItem.text = items.length === 0 ? '✓ webpack' : '✗ webpack'
-    statusItem.isProgress = false
-  })
-
-  task.onStderr(lines => {
-    for (let line of lines) {
-      if (line.match(/webpack\sis\swatching/)) {
-        statusItem.isProgress = true
-        statusItem.text = 'webpack watching'
-      }
-    }
-  })
-
-  context.subscriptions.push(Disposable.create(() => {
-    task.dispose()
-  }))
+    })
+  }
 
   context.subscriptions.push(commands.registerCommand('webpack.watch', async () => {
-    const running = await task.running
-    if (running) {
-      workspace.showMessage(`Webpack is already running`, 'warning')
-      return
+    subscriptions.push(
+      workspace.onDidChangeTextDocument((e) => {
+        const doc = workspace.getDocument(e.textDocument.uri)
+        if (['typescript', 'javascript', 'typescriptreact', 'javascriptreact'].indexOf(doc.textDocument.languageId) !== -1) {
+          if (statusItem) {
+            statusItem.text = 'webpack'
+          }
+        }
+      })
+    )
+    subscriptions.push(
+      commands.registerCommand('webpack.stop', () => {
+        watchSubscription.dispose()
+        if (statusItem) {
+          statusItem.hide()
+        }
+      })
+    )
+    if (task) {
+      const running = await task.running
+      if (running) {
+        workspace.showMessage(`Webpack is already running`, 'warning')
+        return
+      }
     }
+    newWatch()
     const cmd = path.join('./', 'node_modules', '.bin', 'webpack')
     const doc = await workspace.document
     const currentFiePath = Uri.parse(doc.textDocument.uri).fsPath
